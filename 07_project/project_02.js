eab2d0c4071cfa94d4ea7c102bc361642ade93e1
ankit-4623 <some@email.com>
2024-09-07 21:30:22 +0530

    project

---
+++ 07_project/project_02.js
@@ -0,0 +1,28 @@
+const form = document.querySelector('form')
+// if i declare height and weight value here 
+//-> this usecase will give you empty value
+form.addEventListener('submit',function(e){
+e.preventDefault();
+
+const height = parseInt(document.querySelector('#height').value)
+const weight = parseInt(document.querySelector('#weight').value)
+const results = document.querySelector('#results')
+
+if (height===''||height<0||isNaN(height)) {
+    results.innerHTML = "Please give a valid height"
+}
+
+
+else if (weight===''||weight<0||isNaN(weight)) {
+    results.innerHTML = "Please give a valid weight"
+}
+else{
+  const bmi =  ( weight / ((height * height)/10000)).toFixed(2)
+
+  results.innerHTML = `<span>${bmi}</span>`
+}
+
+})
+
+
+
